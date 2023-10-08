9c7e947c40aac9e0602257f2200e7f8aa838e7c9
PullolilEzequiel <ezequielkarottupullolil@gmail.com>
2023-10-08 01:29:59 -0300

    Ahora el player se mueve de forma uniforme

--- Player.gd
+++ Player.gd
@@ -1,15 +1,16 @@
 extends CharacterBody2D
 
-var speed := 60
+var speed := 120
 var direction_x := 0.0
 var direction_y := 0.0
 @onready var sprite := $Sprite2D
 func _physics_process(delta):
-	direction_x = Input.get_axis("ui_left", "ui_right") * 2
-	direction_y = Input.get_axis("ui_up", "ui_down") * 2
+	direction_x = Input.get_axis("ui_left", "ui_right")
+	direction_y = Input.get_axis("ui_up", "ui_down") 
 	
 	velocity.x = direction_x * speed 
 	velocity.y = direction_y * speed 
 	
+	velocity = velocity.normalized() * speed
 	sprite.flip_h = direction_x < 0 if direction_x != 0 else sprite.flip_h
 	move_and_slide()
